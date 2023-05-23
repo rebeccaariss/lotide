a06553455eb29921b09284e70e9fc7b547c16261
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-05-23 16:56:57 -0400

    Initial commit/setup

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const eqArrays = function(arr1, arr2) {
+  let same = false;
+  // compare each index in arr1 to the corresponding index in arr2
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        same = true;
+      } else {
+        same = false;
+        return same;
+      }
+    }
+  }
+  return same;
+};
+
