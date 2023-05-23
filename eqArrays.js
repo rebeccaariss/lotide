c5f226c3a9c9645cf6840868444dc4134721a95f
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-05-23 16:49:37 -0400

    Add function for array comparison and update test cases

--- eqArrays.js
+++ eqArrays.js
@@ -7,5 +7,24 @@ const assertEqual = function(actual, expected) {
 };
 
 // FUNCTION IMPLEMENTATION
+const eqArrays = function(arr1, arr2) {
+  let same = false;
+  // compare each index in arr1 to the corresponding index in arr2
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        same = true;
+      } else {
+        same = false;
+        return same;
+      }
+    }
+  }
+  return same;
+};
 
 // TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
