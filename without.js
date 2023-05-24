4a2e2956786e56caa0389f6ec4117a71f42ecbd6
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-05-23 21:34:53 -0400

    Modify function to remove items from a given array, add test cases

--- without.js
+++ without.js
@@ -1,15 +1,12 @@
 // Copying and pasting this function for now per instructions:
 const assertArraysEqual = function(arr1, arr2) {
   let same = false;
-  // compare each index in arr1 to the corresponding index in arr2
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] === arr2[i]) {
         same = true;
       } else {
         same = false;
-        // this return statement can be eliminated:
-        // return same;
       }
     }
   }
@@ -21,35 +18,42 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 // Copying and pasting this function for now per instructions:
-const eqArrays = function(arr1, arr2) {
-  let same = false;
-  // compare each index in arr1 to the corresponding index in arr2
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        same = true;
-      } else {
-        same = false;
-        return same;
-      }
-    }
-  }
-  return same;
-};
+// (Commented out because did not use):
+// const eqArrays = function(arr1, arr2) {
+//   let same = false;
+//   if (arr1.length === arr2.length) {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] === arr2[i]) {
+//         same = true;
+//       } else {
+//         same = false;
+//         return same;
+//       }
+//     }
+//   }
+//   return same;
+// };
 
 // FUNCTION IMPLEMENTATION
 const without = function(source, itemsToRemove) {
-
+  const updated = [];
+  for (const item of source) {
+    if (!itemsToRemove.includes(item)) {
+      updated.push(item);
+    }
+  }
+  return updated;
 };
 
 // TEST CODE
-without([1, 2, 3], [1]); // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+// Test below currently fails. Why? Related to objects?
+assertArraysEqual(without([], [1]), []);
+assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]);
 
 // Ensure the original array has not been modified
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-// add further tests
