44ef7721f4dead1933874b8c3e7ee6b65930d503
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-05-25 12:50:25 -0400

    Initial commit

---
+++ countOnly.js
@@ -0,0 +1,15 @@
+// Copying and pasting this function for now per instructions:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const countOnly = function(allItems, itemsToCount) {
+
+};
+
+// TEST CODE
