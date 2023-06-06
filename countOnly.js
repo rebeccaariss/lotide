60cda6321bc6660bda1b098b0d1373eb54611706
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-06 11:40:28 -0400

    Starting fresh

--- countOnly.js
+++ countOnly.js
@@ -10,44 +10,12 @@ const assertEqual = function(actual, expected) {
 // FUNCTION IMPLEMENTATION
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
+
+// create and return a new object containing the [keys?] that were to be counted and how many instances of that key were found in the array
+
 const countOnly = function(allItems, itemsToCount) {
-  const toCheck = [];
-  const result = {};
-  // check object key
-  // would it be more appropriate to call the variable here key or value? this is worth asking about - consider what you're accessing here:
-  for (const key in itemsToCount) {
-    if (itemsToCount[key] === true) {
-      toCheck.push(key);
-    }
-  }
-  // if value === true, we should check how many instances of this [key?] there are in the array (allItems)
-  //forEach for doing something to each index in array
-  for (const name of toCheck) {
-    allItems.forEach(element => {
-      if (name === element) {
-        result[name] += 1;
-      }
-    });
-    return Object.keys(result);
-  }
-
-  //this is actually helpful:
-  // const findWaldo = function(names, found) {
-  //   for (let i = 0; i < names.length; i++) {
-  //     let name = names[i];
-  //     if (name === "Waldo") {
-  //       found();   // execute callback
-  //     }
-  //   }
-  // }
-  
-
-  // create and return a new object containing the [keys?] that were to be counted and how many instances of that key were found in the array
-
 
 };
-
-// start to practice implementing arrow functions too re: lecture
 
 // TEST CODE
 const firstNames = [
@@ -62,11 +30,13 @@ const firstNames = [
   "Joe"
 ];
 
-console.log(countOnly(firstNames, firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }));
+// console.log(countOnly(firstNames, firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }));
 
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
+console.log(result1);
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
