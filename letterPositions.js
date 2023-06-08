71001753d5c63d166f8255007d2ae3bab823ce58
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-08 09:12:43 -0400

    Add note for later refactoring

--- letterPositions.js
+++ letterPositions.js
@@ -57,3 +57,8 @@ const letterPositions = function(sentence) {
 // TEST CODE
 assertArraysEqual(letterPositions("hello").e, [1]);
 assertArraysEqual(letterPositions("hello world").l, [2, 3, 8]);
+
+// Note for refactor:
+// Currently we can't use assertArraysEqual to ensure that the argument " " does not create a key in results.
+// (This is because there will be no array created, and this assertion compares arrays).
+// Import assertEqual or write new test code to test for this on refactor.
