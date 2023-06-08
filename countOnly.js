bcd5bbadc9687f1fc428a34884432e9a1be84c39
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-07 22:22:23 -0400

    Add code from Compass

--- countOnly.js
+++ countOnly.js
@@ -14,7 +14,19 @@ const assertEqual = function(actual, expected) {
 // create and return a new object containing the [keys?] that were to be counted and how many instances of that key were found in the array
 
 const countOnly = function(allItems, itemsToCount) {
+  const results = {};
 
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
 };
 
 // TEST CODE
