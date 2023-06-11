bc865298212f0aa1b5baa859f281929fb0ff2807
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-11 01:26:07 -0400

    Modularize/separate tests from assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-// Copying and pasting this function for now per instructions:
-const eqArrays = function(arr1, arr2) {
-  let same = false;
-  // compare each index in arr1 to the corresponding index in arr2
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        same = true;
-      } else {
-        same = false;
-        return same;
-      }
-    }
-  }
-  return same;
-};
+const eqArrays = require('./eqArrays');
 
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(arr1, arr2) {
@@ -24,10 +9,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-// Check message is printing properly:
-// ("Functions that do not return values and instead print out
-// messages are more difficult to test using assertions. This
-// is because we cannot simply compare their return value to
-// an expected value.")
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], ['a', 'b', 'c']);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+// Check message is printing properly:
+// ("Functions that do not return values and instead print out
+// messages are more difficult to test using assertions. This
+// is because we cannot simply compare their return value to
+// an expected value.")
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], ['a', 'b', 'c']);
