94ba706b9d58c4c0f9bb8e579e8aae82038c5de7
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-11 01:35:07 -0400

    Modularize/separate tests from middle function

--- middle.js
+++ middle.js
@@ -1,39 +1,3 @@
-// Copying and pasting this function for now per instructions:
-const assertArraysEqual = function(arr1, arr2) {
-  let same = false;
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        same = true;
-      } else {
-        same = false;
-      }
-    }
-  }
-  if (same === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-// Copying and pasting this function for now per instructions:
-// (Commented out because did not use):
-// const eqArrays = function(arr1, arr2) {
-//   let same = false;
-//   if (arr1.length === arr2.length) {
-//     for (let i = 0; i < arr1.length; i++) {
-//       if (arr1[i] === arr2[i]) {
-//         same = true;
-//       } else {
-//         same = false;
-//         return same;
-//       }
-//     }
-//   }
-//   return same;
-// };
-
 // FUNCTION IMPLEMENTATION
 const middle = function(arr) {
   if (arr.length <= 2) {
@@ -45,17 +9,4 @@ const middle = function(arr) {
   }
 };
 
-// TEST CODE
-// For arrays with one or two elements, there is no middle. Return an empty array.
-// Note: assertArraysEqual function needs to be refactored to
-// handle empty arrays.
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
