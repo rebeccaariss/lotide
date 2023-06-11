b533e80bea8f41ea0c82941ea9e89329cfbe70a8
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-11 00:59:41 -0400

    Modify function to handle empty arrays

--- eqArrays.js
+++ eqArrays.js
@@ -9,6 +9,10 @@ const assertEqual = function(actual, expected) {
 // FUNCTION IMPLEMENTATION
 const eqArrays = function(arr1, arr2) {
   let same = false;
+  if (arr1.length === 0 && arr2.length === 0) {
+    same = true;
+    return same;
+  }
   // compare each index in arr1 to the corresponding index in arr2
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
@@ -28,3 +32,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], []), true);
